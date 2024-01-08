f0907e4e67e8a908124b72e94065b789c404a2b6
daviekim <daviekim.dev@gmail.com>
2024-01-08 04:21:35 -0600

    adds senderUserId and recieverUserId for getMessagesList function in messages repository

--- lib/domain/repositories/messages_repository.dart
+++ lib/domain/repositories/messages_repository.dart
@@ -1,7 +1,10 @@
 import 'package:private_chat/domain/models/message.dart';
 
 abstract class MessagesRepository {
-  Stream<List<Message>> getMessagesList();
+  Stream<List<Message>> getMessagesList({
+    required senderUserId,
+    required receiverUserId,
+  });
 
   Future<bool> sendMessage(Message message);
 
