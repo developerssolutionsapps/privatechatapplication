3d36b3eb019b0705091f64e1547cca78948fb71c
daviekim <daviekim.dev@gmail.com>
2024-01-08 06:21:54 -0600

    adds store file to firebase storage function to facilitate sending of file massages

--- lib/data/repositories/messages_repository_impl.dart
+++ lib/data/repositories/messages_repository_impl.dart
@@ -1,9 +1,13 @@
+import 'dart:io';
+
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_storage/firebase_storage.dart';
 import 'package:private_chat/domain/models/message.dart';
 import 'package:private_chat/domain/repositories/messages_repository.dart';
 
 class MessagesRepositoryImpl implements MessagesRepository {
   final FirebaseFirestore _firestore = FirebaseFirestore.instance;
+  final FirebaseStorage _firebaseStorage = FirebaseStorage.instance;
   @override
   Stream<List<Message>> getMessagesList({
     required senderUserId,
@@ -49,6 +53,21 @@ class MessagesRepositoryImpl implements MessagesRepository {
   @override
   Future<void> sendTextMessage(Message message) async {
     await _saveTextMessage(message: message);
+  }
+
+  Future<String> _storeFileToFirebaseStorage({
+    required File file,
+    required String path,
+    required String fileName,
+  }) async {
+    try {
+      UploadTask imageUploadTask =
+          _firebaseStorage.ref().child(path).child(fileName).putFile(file);
+      TaskSnapshot snapshot = await imageUploadTask;
+      return await snapshot.ref.getDownloadURL();
+    } catch (e) {
+      throw 'Image not found';
+    }
   }
 
   /// invoke to save message data to message sub collection
