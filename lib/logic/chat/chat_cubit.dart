732698b6c42c02b233ad1ab237ff16fc6e236786
daviekim <daviekim.dev@gmail.com>
2024-01-08 11:57:19 -0600

    creates a local function getMessages

--- lib/logic/chat/chat_cubit.dart
+++ lib/logic/chat/chat_cubit.dart
@@ -1,8 +1,26 @@
+import 'dart:io';
+
 import 'package:bloc/bloc.dart';
 import 'package:equatable/equatable.dart';
+import 'package:private_chat/domain/models/message.dart';
+import 'package:private_chat/domain/repositories/messages_repository.dart';
+
+import '../../core/enums/message_type.dart';
 
 part 'chat_state.dart';
 
 class ChatCubit extends Cubit<ChatState> {
-  ChatCubit() : super(ChatInitial());
+  final MessagesRepository _messagesRepository;
+
+  ChatCubit(this._messagesRepository) : super(ChatInitial());
+
+  Stream<List<Message>> getMessages({
+    required String senderUserId,
+    required String receiverUserId,
+  }) {
+    return _messagesRepository.getMessagesList(
+      senderUserId: senderUserId,
+      receiverUserId: receiverUserId,
+    );
+  }
 }
