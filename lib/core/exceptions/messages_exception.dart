eaf858c3b541e01cbd2d046e39705a4aa7285a71
daviekim <daviekim.dev@gmail.com>
2024-01-09 10:47:37 +0300

    creates exceptions for the messages

---
+++ lib/core/exceptions/messages_exception.dart
@@ -0,0 +1,9 @@
+class MessageUpdateFailedException implements Exception {}
+
+class MessageDocumentNotFoundException implements Exception {}
+
+class MessageTimeOutException implements Exception {}
+
+class MessageFetchFailedException implements Exception {}
+
+class MessageCreateFailedException implements Exception {}
