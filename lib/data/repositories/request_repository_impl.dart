b498397080c6ad1f11f4468a7b6d1e2086c4efa3
daviekim <daviekim.dev@gmail.com>
2024-01-09 09:42:21 +0300

    handles exception for get all requests method

--- lib/data/repositories/request_repository_impl.dart
+++ lib/data/repositories/request_repository_impl.dart
@@ -124,13 +124,24 @@ class RequestRepositoryImpl implements RequestRepository {
 
   @override
   Future<List<Request>> getAllRequest() async {
-    List<Request> listReq = [];
-    QuerySnapshot querySnapshot = await firestore.collection("requests").get();
-    for (var doc in querySnapshot.docs) {
-      Request user = Request.fromMap(doc.data() as Map);
-      listReq.add(user);
+    try {
+      List<Request> listReq = [];
+      QuerySnapshot querySnapshot =
+          await firestore.collection("requests").get();
+      for (var doc in querySnapshot.docs) {
+        Request user = Request.fromMap(doc.data() as Map);
+        listReq.add(user);
+      }
+      return listReq;
+    } on FirebaseException catch (e) {
+      if (e.code == "not Found") {
+        throw RequestDocumentNotFoundException();
+      } else {
+        throw RequestFetchFailedException();
+      }
+    } catch (_) {
+      throw RequestFetchFailedException();
     }
-    return listReq;
   }
 
   @override
