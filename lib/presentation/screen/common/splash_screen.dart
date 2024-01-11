6c309de27246c29873befa1392cca60b33ae43e1
daviekim <daviekim.dev@gmail.com>
2024-01-11 21:35:42 +0300

    implement auth checking in spashscreen

--- lib/presentation/screen/common/splash_screen.dart
+++ lib/presentation/screen/common/splash_screen.dart
@@ -1,63 +1,88 @@
-import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
 import '../../../core/app_export.dart';
+import '../../../logic/auth/auth_bloc.dart';
+import '../../routes/path.dart';
+import '../../widgets/custom_overlayentry.dart';
 
-class SplashScreen extends StatelessWidget {
-  const SplashScreen({Key? key})
-      : super(
-          key: key,
-        );
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({super.key});
 
-  static Widget builder(BuildContext context) {
-    Future.delayed(const Duration(milliseconds: 5000), () {
-      NavigatorService.popAndPushNamed(AppRoutes.authWidget);
-    });
-    return SplashScreen();
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    super.initState();
+    WidgetsBinding.instance.addPostFrameCallback((timeStamp) => _checkAuth());
+  }
+
+  _checkAuth() async {
+    context.read<AuthBloc>().add(AuthEventInitialize());
+  }
+
+  _listenAuthState(String id) {
+    context.replace(RoutePath.main);
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
   }
 
   @override
   Widget build(BuildContext context) {
     mediaQueryData = MediaQuery.of(context);
 
-    return SafeArea(
-      child: Scaffold(
-        body: StreamBuilder<User?>(
-            stream: null,
-            builder: (context, snapshot) {
-              return SizedBox(
-                width: double.maxFinite,
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    SizedBox(height: 5.v),
-                    SizedBox(
-                      height: 167.v,
-                      width: 152.h,
-                      child: Stack(
-                        alignment: Alignment.bottomCenter,
-                        children: [
-                          CustomImageView(
-                            imagePath: ImageConstant.imgGroup108,
-                            height: 109.v,
-                            width: 95.h,
-                            fit: BoxFit.scaleDown,
-                            alignment: Alignment.topCenter,
-                            margin: EdgeInsets.only(right: 20.h),
-                          ),
-                          Align(
-                            alignment: Alignment.bottomCenter,
-                            child: Text(
-                              "lbl_private".tr,
-                              style: CustomTextStyles.displayMediumOnPrimary,
-                            ),
-                          ),
-                        ],
+    return BlocListener<AuthBloc, AuthState>(
+      listener: (context, state) {
+        if (state is AuthStateLoggedIn)
+          context.replace(RoutePath.setDisplayName);
+        if (state is Loading) {
+          CustomOverlayEntry.instance.loadingCircularProgressIndicator(context);
+        }
+        if (state is AuthStateLoggedOut) {
+          CustomOverlayEntry.instance.hideOverlay();
+        }
+      },
+      child: SafeArea(
+        child: Scaffold(
+          body: SizedBox(
+            width: double.maxFinite,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                SizedBox(height: 5.v),
+                SizedBox(
+                  height: 167.v,
+                  width: 152.h,
+                  child: Stack(
+                    alignment: Alignment.bottomCenter,
+                    children: [
+                      CustomImageView(
+                        imagePath: ImageConstant.imgGroup108,
+                        height: 109.v,
+                        width: 95.h,
+                        fit: BoxFit.scaleDown,
+                        alignment: Alignment.topCenter,
+                        margin: EdgeInsets.only(right: 20.h),
                       ),
-                    ),
-                  ],
+                      Align(
+                        alignment: Alignment.bottomCenter,
+                        child: Text(
+                          "Private",
+                          style: CustomTextStyles.displayMediumOnPrimary,
+                        ),
+                      ),
+                    ],
+                  ),
                 ),
-              );
-            }),
+              ],
+            ),
+          ),
+        ),
       ),
     );
   }
