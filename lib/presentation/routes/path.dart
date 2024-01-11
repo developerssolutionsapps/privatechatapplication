b633240c4e4f5e53af59d7fdcd8ede57a774c853
daviekim <daviekim.dev@gmail.com>
2024-01-11 21:34:09 +0300

    adds some more paths

--- lib/presentation/routes/path.dart
+++ lib/presentation/routes/path.dart
@@ -1,7 +1,12 @@
 class RoutePath {
   static const String splash = '/';
-  static const String phoneSignIn = '/auth/phone_sign_in';
-  static const String otpCode = '/auth/otp_code';
+  static const String signin = '/sign_in';
+  static const String otpScreen = '/otp_screen';
+  static const String main = '/main';
+  static const String setDisplayName = 'main/set_display_name';
+  static const String setBirthday = 'main/set_birthday';
+  static const String setGender = 'main/set_gender';
+  static const String requestInvite = 'main/request_invite';
 
   static routeName(String path) => path.substring(5);
 }
