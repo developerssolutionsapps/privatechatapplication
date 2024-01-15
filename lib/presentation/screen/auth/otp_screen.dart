b0b386486b773ba998cc680e79d8963b1f3469ab
daviekim <daviekim.dev@gmail.com>
2024-01-15 06:34:10 -0600

    implement otp authentication functionalities

--- lib/presentation/screen/auth/otp_screen.dart
+++ lib/presentation/screen/auth/otp_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
 import 'package:private_chat/presentation/routes/path.dart';
 import '../../../core/app_export.dart';
 import '../../../logic/auth/auth_bloc.dart';
@@ -6,12 +7,8 @@ import '../../widgets/custom_elevated_button.dart';
 import '../../widgets/custom_pin_code_text_field.dart';
 
 class OtpScreen extends StatefulWidget {
-  final String? verificationId;
-  const OtpScreen({super.key, this.verificationId});
-
-  static Widget builder(BuildContext context) {
-    return OtpScreen();
-  }
+  final String verificationId;
+  const OtpScreen({super.key, required this.verificationId});
 
   @override
   State<OtpScreen> createState() => _OtpScreenState();
@@ -35,92 +32,102 @@ class _OtpScreenState extends State<OtpScreen> {
   Widget build(BuildContext context) {
     mediaQueryData = MediaQuery.of(context);
 
-    return SafeArea(
-      child: Scaffold(
-        resizeToAvoidBottomInset: false,
-        body: Container(
-          width: double.maxFinite,
-          padding: EdgeInsets.symmetric(
-            horizontal: 47.h,
-            vertical: 61.v,
-          ),
-          child: Column(
-            children: [
-              CustomImageView(
-                imagePath: ImageConstant.imgGroup108,
-                height: 72.v,
-                width: 63.h,
-              ),
-              SizedBox(height: 7.v),
-              Text(
-                "Private",
-                style: CustomTextStyles.titleLargeAlibabaPuHuiTi20OnPrimary,
-              ),
-              Spacer(
-                flex: 35,
-              ),
-              Align(
-                alignment: Alignment.centerLeft,
-                child: Row(
-                  children: [
-                    CustomImageView(
-                      imagePath: ImageConstant.imgGroup115,
-                      height: 14.v,
-                      width: 11.h,
-                      margin: EdgeInsets.symmetric(vertical: 4.v),
-                    ),
-                    Padding(
-                      padding: EdgeInsets.only(left: 5.h),
-                      child: Text(
-                        "Iput Verification",
-                        style: theme.textTheme.titleMedium,
+    return BlocListener<AuthBloc, AuthState>(
+      listener: (context, state) {
+        if (state is AuthCodeVerificationFailedState) {
+          context.replace(RoutePath.signin);
+        }
+        if (state is Authenticated) {
+          context.replace(RoutePath.main);
+        }
+      },
+      child: SafeArea(
+        child: Scaffold(
+          resizeToAvoidBottomInset: false,
+          body: Container(
+            width: double.maxFinite,
+            padding: EdgeInsets.symmetric(
+              horizontal: 47.h,
+              vertical: 61.v,
+            ),
+            child: Column(
+              children: [
+                CustomImageView(
+                  imagePath: ImageConstant.imgGroup108,
+                  height: 72.v,
+                  width: 63.h,
+                ),
+                SizedBox(height: 7.v),
+                Text(
+                  "Private",
+                  style: CustomTextStyles.titleLargeAlibabaPuHuiTi20OnPrimary,
+                ),
+                Spacer(
+                  flex: 35,
+                ),
+                Align(
+                  alignment: Alignment.centerLeft,
+                  child: Row(
+                    children: [
+                      CustomImageView(
+                        imagePath: ImageConstant.imgGroup115,
+                        height: 14.v,
+                        width: 11.h,
+                        margin: EdgeInsets.symmetric(vertical: 4.v),
                       ),
-                    ),
-                  ],
+                      Padding(
+                        padding: EdgeInsets.only(left: 5.h),
+                        child: Text(
+                          "Iput Verification",
+                          style: theme.textTheme.titleMedium,
+                        ),
+                      ),
+                    ],
+                  ),
                 ),
-              ),
-              SizedBox(height: 1.v),
-              Text(
-                "Verification Code",
-                style: CustomTextStyles.bodyMediumGray500,
-              ),
-              SizedBox(height: 17.v),
-              Padding(
-                padding: EdgeInsets.only(
-                  left: 17.h,
-                  right: 6.h,
+                SizedBox(height: 1.v),
+                Text(
+                  "Verification Code",
+                  style: CustomTextStyles.bodyMediumGray500,
                 ),
-                child: CustomPinCodeTextField(
-                  context: context,
-                  controller: otpController,
-                  onChanged: (value) {
-                    if (value.length == 6) {
-                      context.read<AuthBloc>().add(AuthEventVerifyCode(
-                          verificationId: widget.verificationId,
-                          code: otpController.text));
-                    }
-                  },
+                SizedBox(height: 17.v),
+                Padding(
+                  padding: EdgeInsets.only(
+                    left: 17.h,
+                    right: 6.h,
+                  ),
+                  child: CustomPinCodeTextField(
+                    context: context,
+                    onChanged: (value) {
+                      print(value);
+                      if (value.length == 6) {
+                        context.read<AuthBloc>().add(AuthEventVerifyCode(
+                            verificationId: widget.verificationId,
+                            code: value));
+                      }
+                    },
+                  ),
                 ),
-              ),
-              Spacer(
-                flex: 25,
-              ),
-              CustomElevatedButton(
-                text: "Resend Code",
-                margin: EdgeInsets.only(
-                  left: 43.h,
-                  right: 25.h,
+                Spacer(
+                  flex: 25,
                 ),
-                buttonTextStyle: CustomTextStyles.titleMediumGray200,
-                alignment: Alignment.centerRight,
-                onPressed: () => NavigatorService.popAndPushNamed(
-                  RoutePath.setDisplayName,
+                CustomElevatedButton(
+                  text: "Resend Code",
+                  margin: EdgeInsets.only(
+                    left: 43.h,
+                    right: 25.h,
+                  ),
+                  buttonTextStyle: CustomTextStyles.titleMediumGray200,
+                  alignment: Alignment.centerRight,
+                  onPressed: () => NavigatorService.popAndPushNamed(
+                    RoutePath.setDisplayName,
+                  ),
                 ),
-              ),
-              Spacer(
-                flex: 39,
-              ),
-            ],
+                Spacer(
+                  flex: 39,
+                ),
+              ],
+            ),
           ),
         ),
       ),
