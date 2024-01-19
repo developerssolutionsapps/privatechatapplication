651d4ec38bad042950971864f6d151a87605b96a
daviekim <daviekim.dev@gmail.com>
2024-01-18 21:16:35 -0600

    adds a cubit and state to manage the state of home screen

---
+++ lib/presentation/screen/request/cubit/home_state.dart
@@ -0,0 +1,14 @@
+part of 'home_cubit.dart';
+
+sealed class HomeState extends Equatable {
+  const HomeState();
+
+  @override
+  List<Object> get props => [];
+}
+
+final class HomeInitial extends HomeState {}
+
+final class HomeSentTab extends HomeState {}
+
+final class HomeReceivedTab extends HomeState {}
