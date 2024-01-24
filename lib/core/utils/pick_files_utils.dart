52564f2a381f63b29855f4ec80bb58a17742ae67
daviekim <daviekim.dev@gmail.com>
2024-01-24 10:40:35 -0600

    implmeents pick image from gallery util function

---
+++ lib/core/utils/pick_files_utils.dart
@@ -0,0 +1,26 @@
+import 'dart:io';
+import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
+import 'package:private_chat/presentation/widgets/chat/helper_widgets.dart';
+
+import '../enums/message_type.dart';
+
+/// Invoke to pick image from gallery.
+Future<File?> pickImageFromGallery(BuildContext context) async {
+  File? imageFile;
+
+  try {
+    XFile? xFile = await ImagePicker().pickImage(
+      source: ImageSource.gallery,
+      imageQuality: 80,
+    );
+
+    if (xFile != null) {
+      imageFile = File(xFile.path);
+    }
+  } catch (e) {
+    showSnackBar(context, content: e.toString());
+  }
+
+  return imageFile;
+}
