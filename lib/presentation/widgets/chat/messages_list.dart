2f79a7535417c3b4fe44e6eb3d16e22fe447bc91
daviekim <daviekim.dev@gmail.com>
2024-01-28 23:51:48 -0600

    adds message list in the chat screen

--- lib/presentation/widgets/chat/messages_list.dart
+++ lib/presentation/widgets/chat/messages_list.dart
@@ -2,10 +2,12 @@ import 'package:flutter/material.dart';
 import 'package:flutter/scheduler.dart';
 import 'package:intl/intl.dart';
 import 'package:private_chat/core/app_export.dart';
+import 'package:private_chat/logic/request/request_cubit.dart';
 import 'package:private_chat/logic/user/user_cubit.dart';
 import 'package:private_chat/presentation/widgets/chat/message_card.dart';
 
 import '../../../domain/models/message.dart';
+import '../../../logic/chat/chat_cubit.dart';
 
 class MessagesList extends StatefulWidget {
   const MessagesList({
@@ -41,35 +43,48 @@ class _MessagesListState extends State<MessagesList> {
     context.read<UserCubit>().getMyProfile();
     return StreamBuilder<List<Message>>(
       builder: (context, snapshot) {
-        if (!snapshot.hasData) {
-          return const Center(
-            child: CircularProgressIndicator(
-              color: Colors.black,
-            ),
-          );
-        }
+        // if (!snapshot.hasData) {
+        //   return const Center(
+        //     child: CircularProgressIndicator(
+        //       color: Colors.black,
+        //     ),
+        //   );
+        // }
 
-        // adding callback to new message to scroll to bottom.
-        SchedulerBinding.instance.addPostFrameCallback(
-          (_) => _messagesScrollController.animateTo(
-            _messagesScrollController.position.maxScrollExtent,
-            duration: const Duration(milliseconds: 500),
-            curve: Curves.linear,
-          ),
-        );
+        // // adding callback to new message to scroll to bottom.
+        // SchedulerBinding.instance.addPostFrameCallback(
+        //   (_) => _messagesScrollController.animateTo(
+        //     _messagesScrollController.position.maxScrollExtent,
+        //     duration: const Duration(milliseconds: 500),
+        //     curve: Curves.linear,
+        //   ),
+        // );
 
-        return ListView.builder(
-          controller: _messagesScrollController,
-          itemCount: snapshot.data!.length,
-          itemBuilder: (context, index) {
-            Message message = snapshot.data![index];
+        return BlocBuilder<ChatCubit, ChatState>(
+          buildWhen: (context, state) {
+            return state is ChatMessageListState;
+          },
+          builder: (context, state) {
+            if (state is ChatMessageListState) {
+              return ListView.builder(
+                itemCount: state.messageList.length,
+                itemBuilder: (context, index) {
+                  Message message = state.messageList[index];
 
-            return MessageCard(
-              isSender: message.sender == widget.myUId ? true : false,
-              message: message.message,
-              messageType: message.messageType,
-              time: DateFormat.Hm().format(message.createdAt),
-              onSwipe: () {},
+                  return MessageCard(
+                    isSender: message.sender == widget.myUId ? true : false,
+                    message: message.message,
+                    messageType: message.messageType,
+                    time: DateFormat.Hm().format(message.createdAt),
+                    onSwipe: () {},
+                  );
+                },
+              );
+            }
+            return const Center(
+              child: CircularProgressIndicator(
+                color: Colors.black,
+              ),
             );
           },
         );
